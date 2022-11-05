95974155462a38483475e2ecdf71bf1a45643119
davit <dabashidze@seu.edu.ge>
2022-11-05 16:46:18 +0400

    finished first and second task

---
+++ index.js
@@ -0,0 +1,23 @@
+// davaleba 1
+// let array = [2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8];
+// function positiveNumbersSum(numbers) {
+//   let sum = 0;
+//   for (let index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > 0) {
+//       sum += numbers[index];
+//     }
+//   }
+//   return sum;
+// }
+// console.log(positiveNumbersSum(array));
+// davaleba 2
+// let numbers = [10, 50, 6, 7, 8, 11, 6, 3, 9];
+// function sum (numbersArray) {
+//     let sum = 0;
+//     for (let index = 0; index < numbersArray.length; index += 1) {
+//     sum += numbersArray[index];
+//     }
+//    return sum 
+
+// }
+// console.log(sum(numbers));
